45b998a787ababe0e59a850e4688b607b83033d8
Nicholas Pun <nicholaspun99@gmail.com>
2017-09-09 12:23:19 -0400

    template

--- App.js
+++ App.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      playing: true,
+      guessing: false,
+      keyword: null,
+    }
+  }
+
+  viewSwitcher = () => {
+    if (!this.state.playing) {
+      return (
+        <View style={styles.container}>
+          <Text>Start New Game</Text>
+        </View>
+      )
+    }
+    else if (!this.state.guessing && !this.state.keyword) {
+      return (
+        <View style={styles.container}>
+          <Text>Search Giphy + Set Keyword</Text>
+        </View>
+      )
+    }
+    else if (!this.state.guessing) {
+      return (
+        <View style={styles.container}>
+          <Text>Keyword is Correct, null it and start new game</Text>
+        </View>
+      )
+    }
+    else {
+      return (
+        <View style={styles.container}>
+          <Text>Guess</Text>
+        </View>
+      )
+    }
+  }
+
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return <View style={styles.container}>{this.viewSwitcher()}</View>
   }
 }
 
